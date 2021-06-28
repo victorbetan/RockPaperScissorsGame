8a06429f83317c7df43acec1d02662a7e047e16d
victorbetan <victor@peoplewhothink.com>
2021-06-28 15:54:41 -0500

    Added computerPlay function to assign a random computer choice

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let x = "myself";
+/* 
+this function will create a random number from 0 to 2. It will
+assign a choice of "Rock, Scissors, or Paper" to a variable which will be
+the "computer" part of the game. This is what the user will play against.  
+*/
 
-console.log(`hello ${x}`)
+function computerPlay() {
+    //creates random number from 0 to 2
+    let rndmNum = Math.floor(Math.random() * 3);
+
+    //declared variable which will later be assigned a value depending on the random number
+    let compChoice;
+
+    if (rndmNum === 0) {
+        compChoice = "rock";
+    } else if (rndmNum === 1) {
+        compChoice = "scissors";
+    } else {
+        compChoice = "paper";
+    }
+    //need to delete this later, this is just to see output of random choice.
+    console.log(compChoice);
+}
+
+computerPlay();
