267289303fa40dff60080baf9e0cddd5e073d242
victorbetan <victor@peoplewhothink.com>
2021-06-28 16:41:42 -0500

    added playRound() which checks player against computer

--- index.js
+++ index.js
@@ -12,14 +12,42 @@ function computerPlay() {
     let compChoice;
 
     if (rndmNum === 0) {
-        compChoice = "rock";
+        return compChoice = "rock";
     } else if (rndmNum === 1) {
-        compChoice = "scissors";
+        return compChoice = "scissors";
     } else {
-        compChoice = "paper";
+        return compChoice = "paper";
     }
-    //need to delete this later, this is just to see output of random choice.
-    console.log(compChoice);
 }
 
-computerPlay();
+/*
+This function will take the player selection and the computer selection, compares them and 
+returns a winner depending on the games rules. Paper > rock, rock > scissors, scissors > paper. 
+*/
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return `The computer chose ${computerSelection}. Paper beats rock, you win!`;
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return `The computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}. Sorry you lost!`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `The computer chose ${computerSelection}. ${playerSelection} beats ${computerSelection}, you win!`;
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return `The computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}. Sorry you lost!`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `The computer chose ${computerSelection}. ${playerSelection} beats ${computerSelection}, you win!`;
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return `The computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}. Sorry you lost!`;
+    } else {
+        return  "is this a tie? ENGARD!";
+    }
+}
+//this will change to prompt the user for a choice, and it will need to be case-insensitive
+const playerSelection = "scissors";
+
+//this calls the computerPlay() to assign the returned value of "rock, paper, or scissor" to computerSelection
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection, computerSelection);
